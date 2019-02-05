6cc73dbca707a8e194c319a3a8b0c25b1525d3c1
loup topalian <oup.topalian@gmail.com>
2019-02-05 09:24:49 +0700

    js docs documentation

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import mousetrap from "mousetrap";
 import { useEffect, useState } from "react";
 
 /**
- * @param  {(String | String[])} handlerKey - A key, key combo or array of combos according to Mousetrap documentation.
- * @param  { Function } handlerCallback - A function that is triggered on key combo catch.
+ * Use mousetrap hook
+ *
+ * @param  {(string | string[])} handlerKey - A key, key combo or array of combos according to Mousetrap documentation.
+ * @param  { function } handlerCallback - A function that is triggered on key combo catch.
  */
 export default (handlerKey, handlerCallback) => {
   const [trigger, setTrigger] = useState(false);
