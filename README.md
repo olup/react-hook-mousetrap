504b1479416466eb3de05a1d117ac57be0e366cc
loup topalian <oup.topalian@gmail.com>
2019-02-05 09:03:18 +0700

    Change of api  and documenting

--- README.md
+++ README.md
@@ -5,22 +5,18 @@ A quick react hook to trigger actions on specific key combination. It uses [mous
 Import it like this :
 
 ```
-import mousetrapHook from "react-hook-mousetrap"
+import useMousetrap from "react-hook-mousetrap"
 ```
 
 Then in your component :
 
 ```
-mousetrapHook([
-    {
-      keys: "ctrl+enter", // any mousetrap combo, or array of combo
+// useMousetrap(<Keys>, <Callback>)
+useMousetrap("ctrl+enter", // any mousetrap combo, or array of combo
 
-      action: () => { // triggered on key combo
+      () => { // triggered on key combo
         // whatever. You can use your state in there too.
-      }
-
-    }
-  ]);
+      });
 ```
 
 **NOTE : If you want to trap keys in an `input` or `textarea` you need to add the mousetrap class to the element : `className="mousetrap"`**
--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 import mousetrap from "mousetrap";
 import { useEffect, useState } from "react";
 
-export default handlerMap => {
-  const [index, setIndex] = useState(-1);
+/**
+ * @param  {(String | String[])} handlerKey - A key, key combo or array of combos according to Mousetrap documentation.
+ * @param  { Function } handlerCallback - A function that is triggered on key combo catch.
+ */
+export default (handlerKey, handlerCallback) => {
+  const [trigger, setTrigger] = useState(false);
   useEffect(() => {
-    handlerMap.map((handler, index) => {
-      mousetrap.bind(handler.keys, () => setIndex(index));
-    });
+    mousetrap.bind(handlerKey, () => setTrigger(true));
     return () => {
-      handlerMap.map(handler => {
-        mousetrap.unbind(handler.keys);
-      });
+      mousetrap.unbind(handlerKey);
     };
   }, []);
 
-  useEffect(
-    () => {
-      if (index > -1) {
-        handlerMap[index].action();
-      }
-      return () => {
-        setIndex(-1);
-      };
-    },
-    [index]
-  );
+  useEffect(() => {
+    if (trigger) {
+      handlerCallback();
+    }
+    return () => {
+      setTrigger(false);
+    };
+  }, [trigger]);
 };
